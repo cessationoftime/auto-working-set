b42a0dab64718ded4a24c7514a0753dab6b7ba44
Chris Van Vranken <chris@psyonik.com>
2015-07-06 20:39:12 -0400

    first commit, template from scala ide example plugin

---
+++ psyonik.autoworkingset.plugin.tests/src/psyonik/autoworkingset/plugin/SomeInterestingCodeTest.scala
@@ -0,0 +1,13 @@
+package psyonik.autoworkingset.plugin
+
+import org.junit.Test
+import org.junit.Assert._
+import org.scalaide.core.testsetup.TestProjectSetup
+
+class SomeInterestingCodeTest extends TestProjectSetup("aProject", bundleName= "psyonik.autoworkingset.plugin.tests") {
+
+  @Test
+  def numberOfTypes() {
+  }
+
+}
